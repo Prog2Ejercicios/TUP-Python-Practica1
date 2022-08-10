422343fac3e104502479850746b40140fb2801c7
LuchoSantini <lucho_santini7@outlook.es>
2022-08-10 17:21:26 -0300

    Ej 1 Hecho

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -64,7 +64,9 @@ Restricción: Usar el operador módulo.
 numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
-es_divisible_por_siete = 7 % numero_incalculable
+numDiv = 7
+es_divisible_por_siete = 0
+es_divisible_por_siete = numero_incalculable % numDiv
 # COMPLETAR - FIN
 
 assert es_divisible_por_siete
