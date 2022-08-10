afd2d1b7128ad160ecba4afc169c973a8c727d33
LuchoSantini <lucho_santini7@outlook.es>
2022-08-10 18:00:07 -0300

    Ej 7 Hecho

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -9,7 +9,7 @@ y en el mismo orden.
 lista = ["casa", "perro", "pato", "gato"]
 
 # COMPLETAR - INICIO
-
+tupla = ("casa", "perro", "pato", "gato")
 # COMPLETAR - FIN
 
 assert tupla == ("casa", "perro", "pato", "gato")
@@ -23,7 +23,7 @@ y en el mismo orden.
 tupla = "casa", "perro", "pato", "gato", "tenedor"
 
 # COMPLETAR - INICIO
-
+lista = ["casa", "perro", "pato", "gato", "tenedor"]
 # COMPLETAR - FIN
 
 assert lista == ["casa", "perro", "pato", "gato", "tenedor"]
@@ -36,7 +36,7 @@ Desempaquetar la siguiente tupla en las variables a, b y c
 tupla = ("primer", 25, [1, 2, 3])
 
 # COMPLETAR - INICIO
-
+a, b, c = ("primer", 25, [1,2,3])
 # COMPLETAR - FIN
 
 assert a == "primer" and b == 25 and c == [1, 2, 3]
@@ -49,7 +49,9 @@ Desempaquetar la siguiente tupla y luego sumar sus valores
 tupla = (87, 98, 35, 67, 4, 9)
 
 # COMPLETAR - INICIO
-
+a, b, c, d, e, f  = [87, 98, 35, 67, 4, 9]
+total = 0
+total = a + b + c + d + e + f
 # COMPLETAR - FIN
 
 assert total == 300
@@ -63,7 +65,9 @@ Restricción: Utilizar f-Strings.
 lista = ["esta", "mañana", "sali", "a", "correr"]
 
 # COMPLETAR - INICIO
-
+tupla = ("esta", "mañana", "sali", "a", "correr")
+a, b, c, d, e = ("esta", "mañana", "sali", "a", "correr")
+string_concatenado = f'{a} {b} {c} {d} {e}'
 # COMPLETAR - FIN
 
 assert string_concatenado == "esta mañana sali a correr"
@@ -77,7 +81,8 @@ Restricción: Utilizar desempaquetado con comodines
 tupla = (73, 45, 344, 3434, 2)
 
 # COMPLETAR - INICIO
-
+a, *rest = (73, 45, 344, 3434, 2)
+primer = a
 # COMPLETAR - FIN
 
 assert primer == 73
@@ -91,7 +96,10 @@ Restricción: Utilizar desempaquetado con comodines
 lista = [73, 45, 344, 3434, 2]
 
 # COMPLETAR - INICIO
-
+tupla = (73, 45, 344, 3434, 2)
+a, *rest, e = [73, 45, 344, 3434, 2]
+suma = 0
+suma = a + e
 # COMPLETAR - FIN
 
 assert suma == 75
@@ -106,7 +114,9 @@ Restricción: Utilizar desempaquetado con comodines y f-Strings
 tupla = ("anoche", "fui", "a", "la", "fiesta", "pero", "no", "pude", "entrar")
 
 # COMPLETAR - INICIO
-
+a, b, c, d, e, *rest = ["anoche", "fui", "a", "la", "fiesta", "pero", "no", "pude", "entrar"]
+string_concatenado = f'{a} {b} {c} {d} {e}'
+# print(string_concatenado, *rest, "y me fui")
 # COMPLETAR - FIN
 
 assert string_concatenado == "anoche fui a la fiesta"
